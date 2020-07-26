4941bb4ace4ba04c8b7b184c21e0969ddf776156
ankit <ankit@DESKTOP-A4EU1SK>
2020-07-26 16:44:38 +0530

    Topic

---
+++ SpringBoot/src/main/java/com/SpringBoot/Topics/TopicService.java
@@ -0,0 +1,23 @@
+package com.SpringBoot.Topics;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.Spring.pojo.Topic;
+
+@Service
+public class TopicService {
+	private List<Topic> topic = Arrays.asList(new Topic("spring", "Spring Topic", "Spring Description"),
+			new Topic("java", "Core Java", "Java Description"),
+			new Topic("spring", "Spring Boot", "Spring Boot Description"));
+	public List<Topic> getTopics() {
+		return topic;
+	}
+	
+	public Topic getId(String id) {
+		return topic.stream().filter(t-> t.getId().equals(id)).findFirst().get();
+	}
+
+}
