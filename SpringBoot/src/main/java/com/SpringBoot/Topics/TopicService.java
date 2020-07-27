93fdb573378a1883eaeebf7e64db87ea1daac3f8
ankit <ankit@DESKTOP-A4EU1SK>
2020-07-27 21:39:11 +0530

    Put and delete

--- SpringBoot/src/main/java/com/SpringBoot/Topics/TopicService.java
+++ SpringBoot/src/main/java/com/SpringBoot/Topics/TopicService.java
@@ -1,5 +1,6 @@
 package com.SpringBoot.Topics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,15 +8,31 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topic = Arrays.asList(new Topic("spring", "Spring Topic", "Spring Description"),
+	private List<Topic> topic = new ArrayList<Topic>(Arrays.asList(new Topic("spring", "Spring Topic", "Spring Description"),
 			new Topic("java", "Core Java", "Java Description"),
-			new Topic("spring", "Spring Boot", "Spring Boot Description"));
+			new Topic("spring", "Spring Boot", "Spring Boot Description")));
 	public List<Topic> getTopics() {
 		return topic;
 	}
 	
 	public Topic getId(String id) {
 		return topic.stream().filter(t-> t.getId().equals(id)).findFirst().get();
+	}
+	public void addTopic(Topic topic){
+		this.topic.add(topic);
+	}
+
+	public void update(Topic topic2, String id) {
+		for (int i=0;i<topic.size();i++) {
+			if(topic.get(i).getId().equals(id)) {
+				topic.set(i, topic2);
+				return;
+			}
+		}
+	}
+
+	public void delete(String id) {
+		topic.removeIf(t->t.getId().equals(id));
 	}
 
 }
--- SpringBoot/src/main/java/com/SpringBoot/Topics/TopicsController.java
+++ SpringBoot/src/main/java/com/SpringBoot/Topics/TopicsController.java
@@ -4,21 +4,38 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicsController {
 	@Autowired
 	private TopicService topicService;
+
 	@RequestMapping("/topics")
-	public List<Topic> getTopics(){
+	public List<Topic> getTopics() {
 		return topicService.getTopics();
 	}
-	
+
 	@RequestMapping("topic/{id}")
 	public Topic getTopicBasedOnId(@PathVariable String id) {
 		return topicService.getId(id);
 	}
-	
+
+	@RequestMapping(value = "/addTopic", method = RequestMethod.POST, headers = "Accept=application/json")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+
+	@RequestMapping(value = "/updateTopic/{id}", method = RequestMethod.PUT, headers = "Accept=application/json")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.update(topic, id);
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+	public void deleteTopic(String id) {
+		topicService.delete(id);
+	}
 }
